10fc5039c04a87187498833b88e02a34d76c45ab
Freda Hon <freda.hon@gmail.com>
2020-08-29 20:33:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,10 +14,10 @@ def line(katz_deli)
   end
 end 
 
-def take_a_number(katz_deli, new_name)
-  katz_deli.push(new_name)
-  puts "Welcome, #{new_name}. You are number #{katz_deli.length} in line."
-end
+# def take_a_number(katz_deli, new_name)
+#   katz_deli.push(new_name)
+#   puts "Welcome, #{new_name}. You are number #{katz_deli.length} in line."
+# end
 
 def now_serving(katz_deli)
   if katz_deli.length > 0
